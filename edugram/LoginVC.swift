9f385dccd47140788f01d4f7dd081fd3d11c890e
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-12 10:05:49 +0700

    add LoginVC file!

--- edugram/LoginVC.swift
+++ edugram/LoginVC.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
+    @IBOutlet weak var inputEmail: UITextField!
+    @IBOutlet weak var inputPass: UITextField!
+    @IBOutlet weak var alertLabel: UILabel!
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
