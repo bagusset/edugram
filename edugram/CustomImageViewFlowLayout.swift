ab28c006566a9af3b751ef7bf550fb6421898cf3
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-15 09:13:11 +0700

    add CustomImageViewFlowLayout !

---
+++ edugram/CustomImageViewFlowLayout.swift
@@ -0,0 +1,12 @@
+//
+//  CustomImageViewFlowLayout.swift
+//  edugram
+//
+//  Created by Bagus setiawan on 15/01/21.
+//
+
+import UIKit
+
+class CustomImageViewFlowLayout: UICollectionViewFlowLayout {
+
+}
